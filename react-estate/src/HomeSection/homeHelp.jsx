a83d6e65c6a2b894a0d8689c0f59c4c81cadc5d3
yllkanuredini <58362102+yllkanuredini@users.noreply.github.com>
2024-04-22 19:04:35 +0200

    Create homeHelp.jsx

---
+++ react-estate/src/HomeSection/homeHelp.jsx
@@ -0,0 +1,22 @@
+import "./homeHelp.scss"
+
+function homeHelp(){
+    return(
+        <div className="homeHelp">
+            <div className="HelpCenter">
+                <h2>
+                    Tips for home buyers
+                </h2>
+                <img src="./Img/NeedHelp.jpg" alt="This is a picture" />
+                <p>
+                    Description will be here... description will be here...description will be here...description will be here...description will be here...description will be here...description will be here...description will be here...description will be here....
+                </p>
+                <button>
+                    Need Help?
+                </button>
+            </div>
+        </div>
+    )
+}
+
+export default homeHelp;

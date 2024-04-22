a75e10345074d3897d7bbf7721205ab63759b36c
yllkanuredini <58362102+yllkanuredini@users.noreply.github.com>
2024-04-22 19:01:32 +0200

    Create homeSection.scss

---
+++ react-estate/src/HomeSection/homeSection.scss
@@ -0,0 +1,18 @@
+import "./homeSection.scss"
+
+function homeSection(){
+    return(
+        <div className="homeSection">
+            <p>SO EASY ...</p>
+            <p className="just">just</p>
+            <form>
+                <input type="text" name="Location" placeholder="Country, City or Address" />
+                <button>
+                    <img src="./public\Icons\search-icon.png" alt="search" />
+                </button>
+            </form>
+        </div>
+    )
+}
+
+export default homeSection;

f1953d0a8c02984e45aba1e9ed679b57dd43510b
gentkoshi <155828361+gentkoshi@users.noreply.github.com>
2024-04-25 00:16:38 +0200

    HomeSection Update

--- react-estate/src/HomeSection/homeSection.jsx
+++ react-estate/src/HomeSection/homeSection.jsx
@@ -1,18 +1,34 @@
 import "./homeSection.scss"
 
-function homeSection(){
-    return(
-        <div className="homeSection">
-            <p>SO EASY ...</p>
+function homeSection() {
+  return (
+    <div className="homeSection">
+        <p>SO EASY ...</p>
             <p className="just">just</p>
-            <form>
-                <input type="text" name="Location" placeholder="Country, City or Address" />
-                <button>
-                    <img src="./public\Icons\search-icon.png" alt="search" />
-                </button>
-            </form>
-        </div>
-    )
+      <form>
+        <input type="text" name="location" placeholder="City Location"/>
+        <input
+          type="number"
+          name="minPrice"
+          min={0}
+          max={10000000}
+          placeholder="Min Price"
+          id="minPrice"
+        />
+        <input
+          type="number"
+          name="maxPrice"
+          min={0}
+          max={10000000}
+          placeholder="Max Price"
+          id="maxPrice"
+        />
+        <button>
+          <img src="./public\Icons\search-icon.png" alt="" />
+        </button>
+      </form>
+    </div>
+  );
 }
 
 export default homeSection;
